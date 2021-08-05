e2dfab52c120cebc68ebc206c9e10d57969846cf
uendel.couto <uendel.couto@gmail.com>
2021-08-05 10:25:41 -0300

    adiciona atividades iniciais

--- index.py
+++ index.py
@@ -3,6 +3,20 @@ import pandas as pd
 def get_data(file_name):
     return pd.read_csv(file_name)
 
-baseDados = get_data('data/AG.csv')
+def generatePopulation():
+    # TO DO
+    # generate initial population
+
+def selection():
+    # TO DO
+    # select the best individuals among population
+
+def crossover():
+    # TO DO
+    # receives a couple return a new individual
+
+def fitness():
+    # TO DO
+    # receives a n indidividual and give it a fitness score
 
 print(baseDados)
